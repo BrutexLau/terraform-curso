ac119835af477e3e11312046b8adb42cf52968e3
BrutexLau <tenemosmiedo2013@gmail.com>
2025-08-13 13:18:14 +0200

    Asignacion de valores a las variables

---
+++ practica_5/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">=6.2.0, <6.7.0, !=6.4.0"
+    }
+  }
+  required_version = "~>1.12.0"
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-2"
+  alias = "ohio"
+}
