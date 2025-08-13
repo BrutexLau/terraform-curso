b8f8d25847be5f65bca8cfc7802ab9c68ab82f2e
BrutexLau <tenemosmiedo2013@gmail.com>
2025-08-13 17:19:43 +0200

    Uso bloque Data

--- practica_6/ec2.tf
+++ practica_6/ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "public_instance" {
   ami           = "ami-0de716d6197524dd9"
   instance_type = "t3.micro"
   subnet_id = aws_subnet.public_subnet.id
+  key_name = data.aws_key_pair.key.key_name
   tags = {
     Name = "HelloWorld"
   }
