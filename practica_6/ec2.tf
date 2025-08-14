bf890f069c4405a992ea9b8e6981be022e1af218
BrutexLau <tenemosmiedo2013@gmail.com>
2025-08-14 20:06:52 +0200

    Añado como lanzar un script en instancia EC2

--- practica_6/ec2.tf
+++ practica_6/ec2.tf
@@ -1,10 +1,11 @@
 
 resource "aws_instance" "public_instance" {
-  ami           = "ami-0de716d6197524dd9"
-  instance_type = "t3.micro"
+  ami           = var.ec2_specs.ami
+  instance_type = var.ec2_specs.instance_type
   subnet_id     = aws_subnet.public_subnet.id
   key_name      = data.aws_key_pair.key.key_name
   vpc_security_group_ids = [ aws_security_group.sg_public_instance.id ]
+  user_data = file("scripts/userdata.sh")
   tags = {
     Name = "HelloWorld"
   }
