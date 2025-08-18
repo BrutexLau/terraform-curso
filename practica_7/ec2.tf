c6893bdab9d80b6efb1131dcae29e0b28204bbf2
BrutexLau <tenemosmiedo2013@gmail.com>
2025-08-18 16:57:54 +0200

    Uso for_each

---
+++ practica_7/ec2.tf
@@ -0,0 +1,30 @@
+variable "instancias" {
+  description = "Nombre de las instancias"
+  type        = set(string)
+  default     = ["apache", "mysql", "jumpserver"]
+}
+
+resource "aws_instance" "public_instance" {
+  for_each               = var.instancias
+  ami                    = var.ec2_specs.ami
+  instance_type          = var.ec2_specs.instance_type
+  subnet_id              = aws_subnet.public_subnet.id
+  key_name               = data.aws_key_pair.key.key_name
+  vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
+  user_data              = file("scripts/userdata.sh")
+  tags = {
+    Name = each.value
+  }
+
+  # lifecycle {
+  #   create_before_destroy = true // crea y luego destruye si el apply tiene que hacerlo
+  #   prevent_destroy = true // no va a destruir este recurso
+  #   ignore_changes = [ 
+  #     ami, // ignorará los cambios de la ami, pero no de los demas items
+  #     subnet_id
+  #    ]
+  #    replace_triggered_by = [ 
+  #     aws_subnet.private_subnet // Es para destruir y crear un recurso que quizas esta fuertemente ligado a por ejemplo en este caso la Private_subnet
+  #     ]
+  # }
+}
---
+++ practica_7/vpc.tf
@@ -0,0 +1,96 @@
+resource "aws_vpc" "vpc_virginia" {
+  cidr_block = var.virginia_cidr
+  tags = {
+    "Name" = "vpc_virginia"
+  }
+}
+
+# resource "aws_vpc" "vpc_virginia" {
+#   cidr_block = var.virginia_cidr
+#   tags = {
+#     Name = "VPC_VIRGINIA"
+#     name = "prueba"
+#     env = "Dev"
+#   }
+# }
+
+# resource "aws_subnet" "public_subnet" {
+#   vpc_id = aws_vpc.vpc_virginia.id
+#   cidr_block = var.public_subnet
+#   map_public_ip_on_launch = true
+# }
+
+# resource "aws_subnet" "private_subnet" {
+#   vpc_id = aws_vpc.vpc_virginia.id
+#   cidr_block = var.private_subnet
+# }
+
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc_virginia.id
+  cidr_block              = var.subnets[0]
+  availability_zone       = "us-east-1a" # 👈 fuerza una AZ donde sí existe t3.micro
+  map_public_ip_on_launch = true
+  tags = {
+    "Name" = "Public_Subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.vpc_virginia.id
+  cidr_block        = var.subnets[1]
+  availability_zone = "us-east-1a" # 👈 misma AZ
+  tags = {
+    "Name" = "Private_Subnet"
+  }
+}
+// Para poder llegar desde internet a nuestas instancias EC2
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc_virginia.id
+
+  tags = {
+    Name = "igw vpc virginia"
+  }
+}
+
+resource "aws_route_table" "public_crt" {
+  vpc_id = aws_vpc.vpc_virginia.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "publi crt"
+  }
+}
+
+resource "aws_route_table_association" "crta_public_subnet" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_crt.id
+}
+//Security Group
+resource "aws_security_group" "sg_public_instance" {
+  name        = "Public Instance SG"
+  description = "Allow SSH inbound traffic and ALL egress traffic"
+  vpc_id      = aws_vpc.vpc_virginia.id
+
+  tags = {
+    Name = "Public Instance SG"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.sg_public_instance.id
+  cidr_ipv4         = var.sg_ingress_cidr
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.sg_public_instance.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
