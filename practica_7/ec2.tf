1d1f04c147e6b07644f5ff1aecd5dfeb907204b3
BrutexLau <tenemosmiedo2013@gmail.com>
2025-08-18 17:06:52 +0200

    Condicionales

--- practica_7/ec2.tf
+++ practica_7/ec2.tf
@@ -28,3 +28,16 @@ resource "aws_instance" "public_instance" {
   #     ]
   # }
 }
+
+resource "aws_instance" "moniroting_instance" {
+  count                  = var.enable_monitoring == 1 ? 1 : 0 // Si es "true" count=1, si es "false" cout=0 y no deployaria la instancia
+  ami                    = var.ec2_specs.ami
+  instance_type          = var.ec2_specs.instance_type
+  subnet_id              = aws_subnet.public_subnet.id
+  key_name               = data.aws_key_pair.key.key_name
+  vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
+  user_data              = file("scripts/userdata.sh")
+  tags = {
+    Name = "Monitoreo"
+  }
+}
--- practica_7/variables.tf
+++ practica_7/variables.tf
@@ -33,3 +33,8 @@ variable "ec2_specs" {
   description = "Parametros de la instancia"
   type        = map(string)
 }
+
+variable "enable_monitoring" {
+  description = "Habilita el despliegue d eun servidor de monitore"
+  type        = number
+}
